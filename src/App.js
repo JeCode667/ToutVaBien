d3963c8250a097a85ef6ad1b9d56aae5f545bc15
sosojic <148350844+sosojic@users.noreply.github.com>
2024-12-05 20:57:32 +0100

    consigne

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import PageInformation from "./pages/PageInformation";
 import Quiz from "./pages/Quiz";
@@ -29,3 +29,10 @@ function App() {
 }
 
 export default App;
+
+//mettre le bouton sortir en plus gros ?
+// dans informations mettre les infos sur toutes la largeur de la page
+//dans informations mettre bien le logo et le nom TOUT VA BIEN 
+// faire que le bouton ? en bas à gauche renvoie vers la page d'informations
+//designer et trouver le thème de la page vers laquelle on re dirige quand on clique sur sortir 
+//mettre bien les 2 carrés de réponses dans le quizz (ils sont décalé)
