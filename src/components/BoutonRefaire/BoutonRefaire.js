d648fceae2c939a3660110f859659a68ee3d69a7
Laura Delbreil <ldelbreil@ensc.fr>
2024-12-13 16:06:24 +0100

    [fix] : modification bouton refaire

--- src/components/BoutonRefaire/BoutonRefaire.js
+++ src/components/BoutonRefaire/BoutonRefaire.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./BoutonRefaire.css";
+import { TbReload } from "react-icons/tb";
 
 const BoutonRefaire = () => {
   const navigate = useNavigate();
 
   return (
     <button class="bouton-tvb bleu" onClick={() => window.location.reload()}>
-      <img
-        id="bouton-refaire"
-        src={require("../../img/refaire.png")}
-        alt="refaire"
-      ></img>
+      Refaire 
+      <TbReload />
     </button>
   );
 };
